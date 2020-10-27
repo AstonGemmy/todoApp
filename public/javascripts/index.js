b938cfc979c8640aa9253b97a1fcf90186b084b2
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2020-10-27 12:27:32 +0100

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -17,10 +17,8 @@ let body;
 let url;
 
 window.addEventListener("load",function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
+    window.scrollTo(0,1);
+alert("scrolled");
 });
 
 accordion.forEach(accordion => {
